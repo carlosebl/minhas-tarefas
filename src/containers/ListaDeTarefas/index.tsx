63d0fd41bf71b9325393f3db3c6c728d77e176f1
carlosebl <carlosebl@gmail.com>
2024-03-19 15:50:36 -0300

    enums criados e aplicados

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,24 +1,26 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+import * as enums from '../../utils/enums/Tarefa'
+
 const tarefas = [
   {
     titulo: 'Estudar TypeScript',
     descricao: 'Assistir a aula 3 da EBAC',
-    prioridade: 'importante',
-    status: 'pendente'
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
   },
   {
     titulo: 'Pagar conta de internet',
     descricao: 'Baixar fatura no e-mail',
-    prioridade: 'urgente',
-    status: 'concluída'
+    prioridade: enums.Prioridade.URGENTE,
+    status: enums.Status.CONCLUIDA
   },
   {
     titulo: 'Ir à academia',
     descricao: 'Fazer treino B',
-    prioridade: 'importante',
-    status: 'pendente'
+    prioridade: enums.Prioridade.IMPORTANTE,
+    status: enums.Status.PENDENTE
   }
 ]
 
